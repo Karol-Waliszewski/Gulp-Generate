26dd4928d0ac18663a81b12fe8adcc739c12a4ec
Karol-Waliszewski <karolwaliszewski@gmail.com>
2019-12-10 22:22:59 +0100

    Fix with extension in scss

--- src/templates/partials/sass.js
+++ src/templates/partials/sass.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const MODULE = {
   require: `sass = require("gulp-sass"),`,
-  watch: "gulp.watch(`${srcDir}/styles/**/*.css`, cssDev);",
+  watch: "gulp.watch(`${srcDir}/styles/**/*.scss`, cssDev);",
   code: fs.readFileSync(path.join(__dirname, "../code/sass.js"), "utf8"),
   package: `"gulp-sass": "^3.1.0"`
 };
